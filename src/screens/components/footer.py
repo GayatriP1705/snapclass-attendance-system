46f0f8bb429d81280c4b0458ca8e378f8e457916
Gayatri Palande <gayatripalande01@gmail.com>
2026-08-16 18:08:55 +0530

    streamlit setup + homescreen

---
+++ src/screens/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# def footer_home():
+#     logo_url = "https://i.ibb.co/dsrqHvgm/logo.png"
+#     st.markdown(f"""
+#         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-botton:30px; margin-top:10px">
+#             <img src="{logo_url}" style="height:100px;"/>    
+#             <h1 style="text-align:center; color:#E0E3FF">SNAP<br/>CLASS </h1>
+#         </div>
+# """,unsafe_allow_html=True)
